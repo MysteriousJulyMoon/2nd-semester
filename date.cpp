6d78065dd19163ffa69aa2b0ecb80e8df775f0e8
Julia <150614653+MysteriousJulyMoon@users.noreply.github.com>
2024-01-25 10:28:28 +0200

    Create date.cpp

---
+++ date.cpp
@@ -0,0 +1,59 @@
+#include"func.hpp"
+#include <fstream>
+#include <chrono>
+#include <ctime>
+
+
+int dateday()
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    return now->tm_mday;
+	
+}
+
+int datemon()
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    return now->tm_mon;
+
+}
+
+int dateyear()
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    return now->tm_year;
+
+}
+
+int datehour()
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    return now->tm_hour;
+
+}
+
+int datemin()
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    return now->tm_min;
+
+}
+
+int datesec()
+{
+    std::time_t t = std::time(0);
+    std::tm* now = std::localtime(&t);
+
+    return now->tm_sec;
+
+}
