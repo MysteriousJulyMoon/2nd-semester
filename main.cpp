d22e637933b2acd0a8eaca8095570646d4abce77
Julia <150614653+MysteriousJulyMoon@users.noreply.github.com>
2024-02-06 15:45:58 +0200

    Add files via upload
    2-nd Lab

--- main.cpp
+++ main.cpp
@@ -1,47 +1,78 @@
+﻿
 #include <iostream>
-#include <fstream>
-#include <string>
-#include "date.hpp"
+#include <cmath>
+using namespace std;
+
+
+struct Point 
+{
+	int x;          //Поля
+	int y;
+};
+
+class RectPlus 
+{
+	int m_a, m_b, m_c;
+public:
+	RectPlus() = default;
+	RectPlus(int a, int b, int c)
+	{
+		setA(a);
+		setB(b);
+		setC(c);
+	}
+
+	void setA(int a)
+	{
+		if (a < 0)
+			std::cout << "Negative A" << std::endl;
+		m_a = a;
+	}
+
+	void setB(int b)
+	{
+		if (b < 0)
+			std::cout << "Negative B" << std::endl;
+		m_b = b;
+	}
+
+	void setC(int c)
+	{
+		if (c < 0)
+			std::cout << "Negative C" << std::endl;
+		m_c = c;
+	}
+
+	int Volume()
+	{
+		return m_a * m_b * m_c;
+	}
+
+	int square()
+	{
+		return 2 * (m_a * m_b + m_a * m_c + m_b * m_c);
+	}
+
+	float diagpnal()
+	{
+		return sqrt(m_a * m_a + m_b * m_b + m_c * m_c);
+	}
+
+};
+
+
+
 
 int main()
 {
-    std::ifstream in("input.txt");
-    if (!in.is_open())
-    {
-        std::cout << "Err";
-        return 0;
-    }
+	RectPlus r;
 
-    std::string s; 
-    std::string lasts;
-    std::ofstream out("input.txt", std::ios::app);
+	r.setA(1);
+	r.setB(2);
+	r.setC(3);
+	std::cout << r.Volume() << std::endl;
+	std::cout << r.square() << std::endl;
+	std::cout << r.diagpnal() << std::endl;
 
-
-    while (std::getline(in, s))
-    {
-        lasts = s;
-    }
-    
-    
-    if (!lasts.empty())
-    {
-        int i = 0;
-        std::string number;
-        while (lasts[i] != ' ')
-        {
-            number += lasts[i];
-            i++;
-        }
-        int num = stoi(number);
-        out << '\n' << num+1 << " " << dateyear() + 1900 << "/" << datemon() + 1 << "/" << dateday() << "/" << datehour() << "/" << datemin() << "/" << datesec();
-    }
-    else
-    {
-        out << 1 << " " << dateyear() + 1900 << "/" << datemon() + 1 << "/" << dateday() << "/" << datehour() << "/" << datemin() << "/" << datesec();
-    }
-
-	
 	return 0;
 }
- 
-
