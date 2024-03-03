752540806c2c4dd60d4e423ab05f24dbbe54dbde
Julia <150614653+MysteriousJulyMoon@users.noreply.github.com>
2024-03-03 19:50:19 +0200

    Create Line.hpp

---
+++ Labs 4-th/Line.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace mt
+{
+	class Line
+	{
+		float m_a, m_b;
+		float m_x, m_y;
+		sf::RectangleShape m_rectangle;
+	public:
+		Line() = default;
+		Line(float x, float y, float a, float b)
+		{
+			Setup(x, y, a, b);
+		}
+		void Setup(float x, float y, float a, float b)
+		{
+			m_x = x;
+			m_y = y;
+			m_a = a;
+			m_b = b;
+			m_rectangle.setSize(sf::Vector2f(m_a, m_b));
+			m_rectangle.setPosition(m_x, m_y);
+			m_rectangle.setFillColor(sf::Color::Color(255, 0, 0, 255));
+		}
+		sf::RectangleShape Get()
+		{
+			return m_rectangle;
+		}
+	};
+}
